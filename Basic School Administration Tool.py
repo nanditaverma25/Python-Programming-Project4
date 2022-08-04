af7b0f79005ba9461b2d3d2c8cc0da82dff0bcd4
Nandita Verma <110238854+nanditaverma25@users.noreply.github.com>
2022-08-04 11:09:41 +0530

    Created

--- Basic School Administration Tool.py
+++ Basic School Administration Tool.py
@@ -10,28 +10,30 @@ def write_into_csv(info_list):
             
         writer.writerow(info_list)
             
-if__name__ == '__main__' :
+if __name__ == '__main__' :
    condition = True
-    student_num = 1
+   student_num = 1
+
     
-    while(condition):
+while(condition):
         student_info=input("Enter student information for student #{} in the following format (Name Age  contact_Number E-mai_ID): ".format(student_num))
         
         #split
         student_info_list = student_info.split(' ')
         
-        print("\nThe entered information is -\nName: {}\nAge: {}\nContact_Number: {}\nE-mail_ID: {}" .format(student_info_list[0], student_info_list[1], student_info_list[2], student_info_list[3]))
+        print("\nThe entered information is -\nName: {}\nAge: {}\nContact_Number: {}\nE-mail_ID: {}"
+        .format(student_info_list[0], student_info_list[1], student_info_list[2], student_info_list[3]))
         
         choice_check = input("Is the entered information correct? (yes/no):")
         
-        if choise_check == "yes":
+        if choice_check == "yes":
             write_into_csv(student_info_list)
             
             condition_check = input("Enter (yes/no) if you want to enter information for another student: ")
             if condition_check == "yes":
                 condition = True
                 student_num = student_num +1
-                elif condition_check == "no":
+            elif condition_check == "no":
                     condition = False
-        elif_choice_check =="no":
+        elif choice_check == "no":
             print("\nPlease re-enter the value")
